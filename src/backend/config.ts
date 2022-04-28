6dfd30fb9544dafce059b27b1acd4e3fe2b5072e
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-28 20:29:19 -0300

    Correções repositório e configuração firebase

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,12 +1,14 @@
-import firebase from "firebase"
-import 'firebase/firestore'
+import { getApp, getApps, initializeApp } from "firebase/app"
+import { getFirestore } from 'firebase/firestore'
 
-if (!firebase.apps.length) {
-    firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-    })
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
 }
 
-export default firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const dataBase = getFirestore(app);
+
+export default dataBase
--- src/backend/db/ColecaoCliente.ts
+++ src/backend/db/ColecaoCliente.ts
@@ -1,43 +1,44 @@
-import firebase from "../config";
+import dataBase  from "../config";
+import firestore, { addDoc, collection, deleteDoc, doc, getDoc, getDocs, setDoc } from 'firebase/firestore'
 import Cliente from "../../core/Cliente";
 import ClienteRepositorio from "../../core/ClienteRepositorio";
 
 export default class ColecaoCliente implements ClienteRepositorio {
 
     #conversor = {
-        toFirestore(cliente: Cliente) {
+        toFirestore: (cliente: Cliente) => {
             return {
                 nome: cliente.nome,
                 idade: cliente.idade                
             }
         },
-        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions) : Cliente {
+        fromFirestore: (snapshot: firestore.QueryDocumentSnapshot, options: firestore.SnapshotOptions) => {
             const dados = snapshot.data(options)
             return new Cliente(dados.nome, dados.idade, snapshot.id)
         }
     }
 
+    #colecaoCliente = collection(dataBase, 'clientes').withConverter(this.#conversor)
+
     async salvar(cliente: Cliente) : Promise<Cliente>{
         if (cliente?.id) {
-            await this.colecao().doc(cliente.id).set(cliente)
+            await setDoc(doc(dataBase, 'clientes', cliente.id).withConverter(this.#conversor), cliente)            
             return cliente
         } else {
-            const docRef = await this.colecao().add(cliente)
-            const doc = await docRef.get()
+            const docRef = await addDoc(this.#colecaoCliente, cliente)
+            const doc = await getDoc(docRef)
             return doc.data()
         }        
     }
 
     async excluir(cliente: Cliente) : Promise<void> {
-        return this.colecao().doc(cliente.id).delete()
+        return await deleteDoc(doc(dataBase, 'clientes', cliente.id))
     }
 
     async obterTodos() : Promise<Cliente[]> {
-        const query = await this.colecao().get()
-        return query.docs.map(doc => doc.data()) ?? []
-    }
-
-    private colecao() {
-        return firebase.firestore().collection('clientes').withConventer(this.#conversor)
-    }
+        const clientesCol = this.#colecaoCliente
+        const clientesSnapshot = await getDocs(clientesCol)
+        const clientesList = clientesSnapshot.docs.map((doc) => doc.data()) ?? []
+        return clientesList;
+    }   
 }
